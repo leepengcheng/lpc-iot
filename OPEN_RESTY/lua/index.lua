c8e93ed7a8a923ce000f54d311c9e01b905cc793
leepengcheng <340098816@qq.com>
2018-08-20 00:21:57 +0800

    update

--- OPEN_RESTY/lua/index.lua
+++ OPEN_RESTY/lua/index.lua
@@ -1,1 +1,30 @@
-ngx.say("hello world leepengcheng")
+local redis = require "resty.redis"
+local red = redis:new()
+red:set_timeout(1000) -- 1 sec
+-- or connect to a unix domain socket file listened
+-- by a redis server:
+-- local ok, err = red:connect("unix:/path/to/redis.sock")
+local ok, err = red:connect("192.168.0.104", 6379)
+if not ok then
+    ngx.say("failed to connect: ", err)
+    return
+end
+local res, err = red:get("cat")
+if not res then
+    ngx.say("failed to get cat: ", err)
+else
+    ngx.say("cat: ", res)
+end
+ok, err = red:set("cat", "an animal too")
+if not ok then
+    ngx.say("failed to set cat: ", err)
+    return
+end
+ngx.say("set result: ", ok)
+-- put it into the connection pool of size 100,
+-- with 10 seconds max idle time
+local ok, err = red:set_keepalive(10000, 100)
+    if not ok then
+    ngx.say("failed to set keepalive: ", err)
+    return
+end
